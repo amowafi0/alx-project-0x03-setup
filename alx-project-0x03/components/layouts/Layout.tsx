142ea74e18450c8512d817d71b791385f5fd310d
Ahmed Mowafy <ahmedmowafy74@GMAIL.COM>
2025-06-08 17:25:09 +0300

    fix

---
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -0,0 +1,17 @@
+
+import { ReactNode } from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import { LayoutProps } from "@/interface";
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+    return (
+        <>
+            <Header />
+            <main className="pt-24">{children}</main>
+            <Footer />
+        </>
+    );
+};
+
+export default Layout;
